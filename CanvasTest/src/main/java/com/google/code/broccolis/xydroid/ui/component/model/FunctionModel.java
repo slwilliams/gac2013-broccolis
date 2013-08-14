1e2f4a6d4678fecfa0316970c1042b1c2e81243f
Anda <anda.nenu@gmail.com>
2013-08-14 01:41:51 -0700

    Merge branch 'master' of /Users/demouser/AndroidStudioProjects/gac2013-broccolis with conflicts.

--- CanvasTest/src/main/java/com/google/code/broccolis/xydroid/ui/component/model/FunctionModel.java
+++ CanvasTest/src/main/java/com/google/code/broccolis/xydroid/ui/component/model/FunctionModel.java
@@ -1,98 +1,60 @@
 package com.google.code.broccolis.xydroid.ui.component.model;
 
-import android.graphics.Path;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Point;
 import android.util.Log;
 
 import com.google.code.broccolis.xydroid.util.Constants;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import de.congrace.exp4j.Calculable;
 import de.congrace.exp4j.ExpressionBuilder;
 
-import static java.lang.Math.abs;
-import static java.lang.Math.min;
-
 public class FunctionModel
 {
-    private static final int RADIUS = 5;
     private static final String NAME = "FunctionModel ";
-    private Calculable calculable = null;
-    private List<Point> points;
-    private List<Point> allPoints;
+
+    private int[] points;
+    private int varX;
+    private Point origin;
+    private int length;
+    private Calculable calc = null;
 
     public FunctionModel(String function, Point initialCoordinate, int xMax)
     {
+        origin = initialCoordinate;
+        length = xMax;
         try
         {
-            int x = initialCoordinate.x;
-            calculable = new ExpressionBuilder(function).withVariable("x", 0).build();
-            points = new ArrayList<Point>();
-            for (int i = 0; i < xMax; i++)
+            calc = new ExpressionBuilder(function).withVariable("x", varX).build();
+            int diff = length - origin.x;
+            points = new int[diff];
+            for(int i = 0; i < diff; i ++)
             {
-                points.add(new Point(x + i, (int) calculable.calculate(x + i)));
+                calc.setVariable("x", i);
+                points[i] = (int)calc.calculate();
             }
-            allPoints = new ArrayList<Point>(points);
         }
         catch (Exception e)
         {
             Log.e(Constants.TAG, NAME + "can't parse the function", e);
         }
-        optimizePoints();
     }
 
-    private void optimizePoints()
+    public void draw(Canvas canvas, Paint paint)
     {
-        int i = 0;
-        while (i + 2 < points.size())
+        //draw self using canvas.path here
+        paint.setColor(Color.BLACK);
+        for(int i = 0; i < points.length-1; i ++)
         {
-            Point p1 = points.get(i);
-            Point p2 = points.get(i + 1);
-            Point p3 = points.get(i + 2);
-
-            float a = (((float) p1.y - (float) p3.y)) / ((float) p1.x - (float) p3.x);
-            float b = (float) p1.y - a * (float) p1.x;
-
-            if (p2.x * a + b == p2.y)
-            {
-                points.remove(i + 1);
-            }
-            else
-            {
-                i++;
-            }
+            canvas.drawLine(origin.x+i,origin.y+points[i], origin.x+i+1, origin.y+points[i+1], paint);
         }
-    }
-
-    public Path getPath()
-    {
-        Path path = new Path();
-
-        for (Point point : points)
-        {
-            path.lineTo(point.x, point.y);
-        }
-
-        return path;
     }
 
     public int getGrad(int x)
     {
-        double h = (double) x * 1e-8;
-        return (int) ((calculable.calculate(x + h) - calculable.calculate(x)) / h); //TODO its just approximation, can implement better algorithm
-    }
-
-    public boolean contains(int x, int y)
-    {
-        for (Point point : allPoints)
-        {
-            if (min(abs(point.x - x), abs(point.y - y)) < RADIUS)
-            {
-                return true;
-            }
-        }
-        return false;
+        // derivative
+        return 0;
     }
 }

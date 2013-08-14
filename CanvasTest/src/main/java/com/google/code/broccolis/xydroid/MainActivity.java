5579490b189291481f26098ede04cd74e43dc958
Scott <darkskillmail@gmail.com>
2013-08-14 08:47:47 -0700

    Wiring up ui.

--- CanvasTest/src/main/java/com/google/code/broccolis/xydroid/MainActivity.java
+++ CanvasTest/src/main/java/com/google/code/broccolis/xydroid/MainActivity.java
@@ -1,34 +1,30 @@
 package com.google.code.broccolis.xydroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.View;
 
 import com.google.code.broccolis.xydroid.ui.view.DrawView;
 
 public class MainActivity extends Activity
 {
-    private DrawView drawView;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
 
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        int width = size.x;
-        int height = size.y;
+        setContentView(R.layout.start_interface);
+    }
 
-        DrawView.height = height;
-        DrawView.width = width;
-
-        drawView = new DrawView(this);
-
-        drawView.setBackgroundColor(Color.WHITE);
-        setContentView(drawView);
+    public void newGame(View view)
+    {
+        Intent intent = new Intent(this, PlayLevel.class);
+        startActivity(intent);
     }
 }
---
+++ CanvasTest/src/main/java/com/google/code/broccolis/xydroid/PlayLevel.java
@@ -0,0 +1,35 @@
+package com.google.code.broccolis.xydroid;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+import com.google.code.broccolis.xydroid.ui.view.DrawView;
+
+import com.google.code.broccolis.xydroid.ui.view.DrawView;
+
+
+public class PlayLevel extends Activity
+{
+    private DrawView drawView;
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+
+        DrawView.height = height;
+        DrawView.width = width;
+
+        drawView = new DrawView(this);
+
+        drawView.setBackgroundColor(Color.WHITE);
+        setContentView(drawView);
+    }
+}

544c2aa6216b70e597988d5b5d9fd6c0df1ebbab
Anda <anda.nenu@gmail.com>
2013-08-15 09:56:47 -0700

    about page

---
+++ CanvasTest/src/main/java/com/google/code/broccolis/xydroid/about.java
@@ -0,0 +1,17 @@
+package com.google.code.broccolis.xydroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by demouser on 8/15/13.
+ */
+
+public class About extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}

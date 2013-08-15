6b0ad232cfb65d32ef72a5eb6cb1fdbe72bb9a8c
Scott <darkskillmail@gmail.com>
2013-08-15 08:48:06 -0700

    Player no longer jumps when sliding out drawer, increased shake_delta.

--- CanvasTest/src/main/java/com/google/code/broccolis/xydroid/ui/view/DrawView.java
+++ CanvasTest/src/main/java/com/google/code/broccolis/xydroid/ui/view/DrawView.java
@@ -49,7 +49,7 @@ public class DrawView extends View
     private ArrayList<FunctionView> functions = new ArrayList<FunctionView>();
     private EditText alertDialogInput;
     private AlertDialog alertDialog;
-    private final double SHAKE_DELTA = 1;
+    private final double SHAKE_DELTA = 1.5;
     private double then;
 
     public DrawView(Context context)
@@ -243,8 +243,11 @@ public class DrawView extends View
         }
         else if (!falling)
         {
-            jumping = true;
-            jumpBase = player.getY();
+            if(eventX >= 75)
+            {
+                jumping = true;
+                jumpBase = player.getY();
+            }
         }
 
         return true;

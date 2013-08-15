9fdc874d45a56bf9e014cbc488ebcc63cf3e50e3
Anda <anda.nenu@gmail.com>
2013-08-15 07:48:56 -0700

    select first level

--- CanvasTest/src/main/java/com/google/code/broccolis/xydroid/MainActivity.java
+++ CanvasTest/src/main/java/com/google/code/broccolis/xydroid/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import static com.google.code.broccolis.xydroid.util.DeviceDependantVariables.SCREEN_HEIGHT;
 import static com.google.code.broccolis.xydroid.util.DeviceDependantVariables.SCREEN_WIDTH;
